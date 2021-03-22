5a52d55e5f3ac3fbeb2298865b79ff13227b5a0e
angiecortesdev <angiecortes29@gmail.com>
2021-03-22 18:51:44 -0500

    pruebas jest finalizadas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 
 // console.log(validator);
+
+
+let tarjeta = document.getElementById("card");
+let ingreso = document.getElementById("ingresar");
+let boton = document.getElementById("ingresar");
+
+
+boton.addEventListener('click', () => {
+    alert('hola')
+});
+
+ingreso.addEventListener('click', () => {
+    let open = validator.isValid(tarjeta.value)
+    let open2 = validator.maskify(tarjeta.value)
+
+    if (open) {
+        alert(`su ${open} es valida`)
+    } else {
+        alert(`su ${open2} NO es valida`)
+    }
+
+})
+
+
+//SOLO LA PALABRA BANK
+let elemento = document.createElement("h2")
+    //crrear texto
+let contenido = document.createTextNode("MANCHESTER CITY")
+    //agregar el texto al documneto
+elemento.appendChild(contenido)
+    //Agregar elemento al documento HTML
+document.querySelector("#imagenM").appendChild(elemento)
+    //setAttributo recibe como parámetro un atributo y su respectivo valor
+    //variable.setAttribute("atributo, "valor")
+elemento.setAttribute("class", "white")
+
+
+
+//PALABRA CARD CREDIT
+
+// let credito = document.createElement("h3");
+// // let tarjeta = document.createTextNode("PASS V.I.P");
+// credito.appendChild(tarjeta);
+// document.getElementById("padre").appendChild(credito);
+// credito.setAttribute("class", "white2");
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,83 @@
 const validator = {
-  // ...
+    isValid: creditCardNumber => {
+        /* En esta expresión regular validaremos que se ingresen los números de 0 al 9*/
+        /* El + evalua si hay mas numeros en la cadena, El método test() ejecuta la búsqueda de una ocurrencia entre una expresión regular y una cadena especificada. Devuelve true o false.*/
+
+
+        if (/[0/9]+/.test(!creditCardNumber) || creditCardNumber == "") return false
+
+        let suma = 0;
+        let procesarNumero = false
+            // console.log(typeof creditCardNumber)
+
+        for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+            /*char charAt(int index) Devuelve el carácter indicado como índice. El primer carácter de la cadena será el del índice 0. Junto con el método . length() podemos recuperar todos los caracteres de la cadena de texto. */
+            let caracter = creditCardNumber.charAt(i);
+
+            /* El parseInt convierte nuestro string a un entero */
+            let numeroEntero = parseInt(caracter, 10);
+
+            // if (i % 2 == 1) {
+            //     console.log(numeroEntero) //multiplcar  *2
+            // }
+
+            //en numero entero vale 8
+            //numerosmultiplicados = true
+
+            // if (numerosMultiplicados && (numeroEntero *= 2) > 9) numeroEntero -= 9
+
+            if (procesarNumero) {
+                // numeroEntero *= 2 
+                numeroEntero = numeroEntero * 2
+                if (numeroEntero > 9) {
+                    // numeroEntero -= 9
+                    numeroEntero = numeroEntero - 9
+                }
+            }
+
+            suma += numeroEntero;
+            procesarNumero = !procesarNumero;
+            // console.log(numeroEntero)
+        }
+
+        // if (suma)
+        // return (suma % 10) == 0;
+        // console.log(suma % 10)
+        // console.log(suma)
+
+        let residuo = suma % 10
+
+        if (residuo == 0) {
+            return true;
+            // console.log('la tarjeta es valida')
+        } else {
+            return false;
+            // console.log('Su tarjeta No es valida')
+        }
+    },
+
+    maskify: creditCardNumber => {
+        let simboloEnmascarar = '#';
+
+        if (creditCardNumber.length >= 4) {
+            let ultimosDigitos = creditCardNumber.slice(-4);
+            let enmascarar = simboloEnmascarar.repeat(creditCardNumber.length - 4) + ultimosDigitos;
+
+            return creditCardNumber = enmascarar;
+        } else {
+            return creditCardNumber;
+        }
+
+    }
+
+
 };
+
+// let resultado1 = validator.isValid('4083952015263')
+// let resultado2 = validator.isValid('4083952015264')
+// let resultado3 = validator.isValid('123456789');
+
+// console.log(resultado1, resultado2, resultado3)
+
 
 export default validator;
